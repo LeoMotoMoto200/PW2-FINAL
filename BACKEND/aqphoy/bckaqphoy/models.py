398a5cc88857039ea9ef66bd7012993e265fe545
GiovanniMDev <gmejiar@unsa.edu.pe>
2025-07-04 22:52:01 -0500

    configurando los settings para usarlo en el proyecto y creando un modelo primitivo de los eventos

--- BACKEND/aqphoy/bckaqphoy/models.py
+++ BACKEND/aqphoy/bckaqphoy/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+    titulo= models.CharField(max_length=100, blank=False)
+    descripción= models.TextField(max_length=200)
+    fecha= models.DateField()
+    hora= models.TimeField()
+    imagen = models.ImageField()
+
+    def __str__(self):
+            return self.titulo
