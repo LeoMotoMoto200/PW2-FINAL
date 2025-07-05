72f76185b2a0fef53146cb1173001dbf30650b33
GiovanniMDev <gmejiar@unsa.edu.pe>
2025-07-04 22:59:48 -0500

    creando el modelo clase Categoria

--- BACKEND/aqphoy/bckaqphoy/models.py
+++ BACKEND/aqphoy/bckaqphoy/models.py
@@ -9,3 +9,10 @@ class Evento(models.Model):
 
     def __str__(self):
             return self.titulo
+    
+class Categoria(models.Model):
+    nombre= models.TextField(max_length=20)
+    color = models.CharField(max_length=7, default="#000000")
+
+    def __str__(self):
+            return self.nombre
