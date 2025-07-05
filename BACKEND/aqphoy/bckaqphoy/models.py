1a945e543bbafc67f598c22327515fdb71f1cd57
GiovanniMDev <gmejiar@unsa.edu.pe>
2025-07-04 23:10:37 -0500

    agregando una relacion muchos a muchos entre eventos y categorias, y tambien que se ordenen por fecha mas proxima y hora mas proxima

--- BACKEND/aqphoy/bckaqphoy/models.py
+++ BACKEND/aqphoy/bckaqphoy/models.py
@@ -1,18 +1,23 @@
 from django.db import models
-
-class Evento(models.Model):
-    titulo= models.CharField(max_length=100, blank=False)
-    descripción= models.TextField(max_length=200)
-    fecha= models.DateField()
-    hora= models.TimeField()
-    imagen = models.ImageField(upload_to='eventos/', default='eventos/default.jpg')
-
-    def __str__(self):
-            return self.titulo
     
 class Categoria(models.Model):
-    nombre= models.TextField(max_length=20)
+    nombre= models.CharField(max_length=20)
     color = models.CharField(max_length=7, default="#000000")
 
     def __str__(self):
             return self.nombre
+
+class Evento(models.Model):
+    titulo= models.CharField(max_length=100, blank=False)
+    descripcion= models.TextField()
+    fecha= models.DateField()
+    hora= models.TimeField()
+    imagen = models.ImageField(upload_to='eventos/', default='eventos/default.jpg')
+    categoria = models.ManyToManyField(Categoria, verbose_name="Categorías del evento")
+
+    class Meta:
+        ordering = ['-fecha', '-hora'] 
+
+    def __str__(self):
+            return self.titulo
+    
