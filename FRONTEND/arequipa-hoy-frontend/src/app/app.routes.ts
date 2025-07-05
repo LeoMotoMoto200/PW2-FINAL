a9dabaf047d1eacca644f996adeaa88a4ff430af
ChipanaGerman <gchipanaj@unsa.edu.pe>
2025-07-04 23:32:32 -0500

    Configurando rutas para modulo Eventos y componentes principales

--- FRONTEND/arequipa-hoy-frontend/src/app/app.routes.ts
+++ FRONTEND/arequipa-hoy-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'eventos', pathMatch: 'full' },
+    { path: 'eventos', loadChildren: () => import('./eventos/eventos-module').then(m => m.EventosModule) }
+];
--- FRONTEND/arequipa-hoy-frontend/src/app/eventos/eventos-routing-module.ts
+++ FRONTEND/arequipa-hoy-frontend/src/app/eventos/eventos-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoEventos } from './listado-eventos/listado-eventos';
+import { DetalleEvento } from './detalle-evento/detalle-evento';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListadoEventos },
+  { path: 'evento/:id', component: DetalleEvento },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
