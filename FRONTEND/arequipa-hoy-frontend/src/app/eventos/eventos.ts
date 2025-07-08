7734e23f98702a6b13c9c685270428c286f43808
ChipanaGerman <gchipanaj@unsa.edu.pe>
2025-07-07 19:29:56 -0500

    Implementar servicio Angular para consumir API de eventos y descargar PDF

--- FRONTEND/arequipa-hoy-frontend/src/app/eventos/eventos.ts
+++ FRONTEND/arequipa-hoy-frontend/src/app/eventos/eventos.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Eventos {
+  private apiUrl = '/api/eventos/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getEventos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getEvento(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${id}/`);
+  }
+
+  descargarPDF(id: number): Observable<Blob> {
+    return this.http.get(`/api/eventos/${id}/pdf/`, { responseType: 'blob' });
+  }
 }
